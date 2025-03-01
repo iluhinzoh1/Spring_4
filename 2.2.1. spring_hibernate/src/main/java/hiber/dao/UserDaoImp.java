355a3464075010fb44666098031d885d546a3637
iluhinzoh1 <ilyanovikov007@mail.ru>
2025-03-01 18:26:31 +0300

    вроде все сделал

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -11,19 +11,28 @@ import java.util.List;
 @Repository
 public class UserDaoImp implements UserDao {
 
-   @Autowired
-   private SessionFactory sessionFactory;
+    @Autowired
+    private SessionFactory sessionFactory;
 
-   @Override
-   public void add(User user) {
-      sessionFactory.getCurrentSession().save(user);
-   }
+    @Override
+    public void add(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
 
-   @Override
-   @SuppressWarnings("unchecked")
-   public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
-      return query.getResultList();
-   }
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> listUsers() {
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        return query.getResultList();
+    }
+
+    @Override
+    public User get(int series, String model) {
+        String hql = "FROM User where car.model=:model AND car.series=:series";
+        TypedQuery<User> userTypedQuery = sessionFactory.getCurrentSession().createQuery(hql, User.class);
+        userTypedQuery.setParameter("series", series);
+        userTypedQuery.setParameter("model", model);
+        return userTypedQuery.getSingleResult();
+    }
 
 }
